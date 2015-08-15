6c1ac313463d964f672d03b39acd9d73d8bc7b55
George Lesica <george@lesica.com>
2015-08-14 20:28:36 -0600

    Echo nvim symlinnks.

--- install.sh
+++ install.sh
@@ -9,12 +9,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo 'Linking .vim'
 mv ~/.vim ~/.vim.bak
 ln -s $DIR/vim ~/.vim
+
+echo 'Linking .nvim'
 mv ~/.nvim ~/.nvim.bak
 ln -s $DIR/vim ~/.nvim
 
 echo 'Linking .vimrc'
 mv ~/.vimrc ~/.vimrc.bak
 ln -s $DIR/vimrc ~/.vimrc
+
+echo 'Linking .nvimrc'
 mv ~/.nvimrc ~/.nvimrc.bak
 ln -s $DIR/vimrc ~/.nvimrc
 
