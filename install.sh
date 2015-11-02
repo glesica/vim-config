34eb9f6f2f9a7333b99709e8339fc09d74d72850
George Lesica <george@lesica.com>
2015-11-01 22:01:41 -0700

    Update Neovim config paths.

--- install.sh
+++ install.sh
@@ -6,13 +6,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir -p ~/.config
+
 echo 'Linking .vim'
 mv ~/.vim ~/.vim.bak
 ln -s $DIR/vim ~/.vim
 
 echo 'Linking .nvim'
 mv ~/.nvim ~/.nvim.bak
-ln -s $DIR/vim ~/.nvim
+ln -s $DIR/vim ~/.config/nvim
 
 echo 'Linking .vimrc'
 mv ~/.vimrc ~/.vimrc.bak
@@ -20,7 +22,7 @@ ln -s $DIR/vimrc ~/.vimrc
 
 echo 'Linking .nvimrc'
 mv ~/.nvimrc ~/.nvimrc.bak
-ln -s $DIR/vimrc ~/.nvimrc
+ln -s $DIR/vimrc ~/.config/nvim/init.vim
 
 echo 'Linking .gvimrc'
 mv ~/.gvimrc ~/.gvimrc.bak
